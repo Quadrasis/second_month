3952dae257fa5eb390e48ee028e0caa9fee63eb8
jalil <mmahmudd@icloud.com>
2022-03-18 09:40:54 +0000

    DURKA

---
+++ lesson 3/urok/main.js
@@ -0,0 +1,17 @@
+let de= [12,23]
+let be = [13,23,33]
+function square(){
+    if (de.length < be.length) {
+         alert(`первый меньше`)
+} else if (de.length > be.length) {
+   alert(`первый больше`)
+} else {
+   alert(`равны`)
+}
+}
+square(de,be)
+let ga = 'fhdjfdjfhdjfhd'
+function countstar(ga) {
+    console.log(ga.length)
+}
+countstar(ga)
