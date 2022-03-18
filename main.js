f33f927b56d88a624bd3638202293c815614a48b
Quadrasis <90049093+Quadrasis@users.noreply.github.com>
2022-03-18 16:39:36 +0000

    domaska 3

---
+++ main.js
@@ -0,0 +1,23 @@
+const num=document.querySelector("#number");
+const plus =document.querySelector("#plus");
+const minus = document.querySelector("#minus");
+const reset = document.querySelector("#reset");
+
+
+let schyotchik = 0;
+
+plus.onclick = function() {
+    schyotchik++;
+    num.innerHTML=schyotchik;
+}
+minus.onclick = function() {
+    schyotchik--;
+    if (schyotchik <1){
+        schyotchik=0;
+    }
+
+}
+reset.onclick = function() {
+    schyotchik = 0;
+    num.innerHTML=schyotchik;
+}
